f5bfdeb72ba2ea73bba9fa4bead270629692d806
Emil Avara <emilavara@hotmail.com>
2024-12-05 16:59:51 +0100

    fixed stars rating count

--- src/index.js
+++ src/index.js
@@ -11,21 +11,25 @@ fetchProducts().then(data => {
 
 function renderProducts() {
     products.forEach((product) => {
-        console.log(product.id)
         let div = document.createElement('a')
         div.className = 'card ea-col-3 ea-col-xs-12';
-        div.href = 'product.html/?id=' + product.id
+        div.href = '/product.html?id=' + product.id
+
+        const determineStars = () => {
+            let rating = Math.round(product.rating.rate)
+            return 'star-count-' + rating
+        }
 
         div.innerHTML = `
             <div class="card-category">${product.category}</div>
-            <img class="card-image" src="${product.image}">
+            <img loading="lazy" class="card-image" src="${product.image}">
             <div class="card-text-container">
                 <h2 class="card-title h4">${product.title}</h2>
                 <hr>
                 <div class="card-price-rating-container">
                     <h3 class="card-price h3">$${product.price}</h3>
                     <div class="card-rating">
-                        <div class="card-star-container">
+                        <div class="card-star-container ${determineStars()}">
                             <div class="star">
                                 <i class="bi bi-star-fill"></i>
                             </div>
