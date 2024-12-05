ac7bc354ef2e3d00bbbb68b362933ecc9d9ba007
Emil Avara <emilavara@hotmail.com>
2024-12-05 19:27:27 +0100

    started work on single product page

--- src/product.js
+++ src/product.js
@@ -1,4 +1,5 @@
 import { fetchProducts } from "./main.js";
+const productSection = document.getElementById('section-product')
 
 let products = []
 
@@ -8,5 +9,54 @@ fetchProducts().then(data => {
 })
 
 function renderProduct() {
+    let params = new URLSearchParams(window.location.search)
+    let id = parseInt(params.get('id')) - 1;
+    let product = products[id];
+
+    const imageContainer = document.createElement('div');
+    const infoContainer = document.createElement('div');
+
+    const determineStars = () => {
+        let rating = Math.round(product.rating.rate)
+        return 'star-count-' + rating
+    }
     
+    imageContainer.classList = 'product-image-container ea-cs-3 ea-ce-7';
+    infoContainer.classList = 'product-info-container ea-cs-7 ea-ce-11'
+
+    imageContainer.innerHTML = `
+        <img src="${product.image}">
+    `;
+
+    infoContainer.innerHTML = `
+        <div class="top-container">
+            <div class="product-category">MEN'S CLOTHING</div>
+            <h1 class="product-title">${product.title}</h1>
+            <div class="product-rating-container">
+                <div class="product-star-container ${determineStars()}">
+                    <div class="star">
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                    <div class="star">
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                    <div class="star">
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                    <div class="star">
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                    <div class="star">
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                </div>
+                <h6 class="product-rating-amount">(${product.rating.count})</h6>
+            </div>
+        </div>
+        <h1 class="product-price">$${product.price}</h1>
+        <button class="sss-btn primary">Add to cart</button>
+    `
+
+    productSection.appendChild(imageContainer)
+    productSection.appendChild(infoContainer)
 }
